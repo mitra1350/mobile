623ed2f0750e2bddce740ab1135f1498bd89d9e3
mitra1350 <mitra.mojdeh72@gmail.com>
2018-05-24 11:28:20 +0400

    login

--- App.js
+++ App.js
@@ -1,47 +1,17 @@
 import React,{Component} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Header from './header'
+import styles from  './styles/style'
+
 
 export default class App extends Component {
-  constructor(){
-    super();
-    this.state={
-      name:"mitra",
-      clock: 0
-    };
-  }
-
-  componentDidMount(){
-    this.setState({
-      name:"ninja"
-    });
-    this.interval = setInterval(()=>this.changClock(), 1000);
-  }
-changClock(){
-  this.setState(prevState => this.setState({
-    clock: prevState.clock + 1
-  }));
-}
 
   render() {
     return (
       <View style={styles.container}>
-        <Text>mitra</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-        <Text>{this.state.name}</Text>
-        <Text>{this.state.clock}</Text>
-        <Header mashin="BMW" motor="hyundai"/>
+        <Text style={styles.Header}>login</Text>
+
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'pink',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ styles/style.js
@@ -0,0 +1,15 @@
+import {StyleSheet} from "react-native"
+
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    backgroundColor: "pink",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  Header:{
+    fontWeight:"bold",
+    fontSize:30
+  }
+});
+export default styles;
